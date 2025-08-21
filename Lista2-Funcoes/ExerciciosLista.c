9a7b9c289609d796b3e35956f4cc6f861e03ed7e
Vinicius Arruda <arruda.vinicius@ufn.edu.br>
2025-08-21 13:49:41 -0300

    Atualiza exercícios da Lista2-Funcoes

--- Lista2-Funcoes/ExerciciosLista.c
+++ Lista2-Funcoes/ExerciciosLista.c
@@ -50,6 +50,50 @@ int main(){
 
 }*/
 
+//3. Escrever um programa que leia dois números e a seguir realize as operações aritméticas básicas
+//(+, -, *, /) sobre estes números. Cada uma das operações deve ser implementada em uma função diferente. 
+
+/*#include <stdio.h>
+
+    soma(int n1, int n2){
+        return n1 + n2;
+
+    }
+
+    subtracao(int n1, int n2){
+        return n1 - n2;
+    }
+
+    multiplicacao(int n1, int n2){
+        return n1 * n2;
+    }
+
+    divisao(int n1, int n2){
+        if(n2 != 0)
+            return n1 / n2;
+        else
+            return 0; // Divisão por zero
+    }
+
+int main(){
+
+    int n1, n2;
+
+    printf("Digite um numero:");
+    scanf("%d", &n1);
+
+    printf("Digite outro numero:");
+    scanf("%d", &n2);
+
+    printf("Soma: %d\n", soma(n1, n2));
+    printf("Subtracao: %d\n", subtracao(n1, n2));
+    printf("Multiplicacao: %d\n", multiplicacao(n1, n2));
+    printf("Divisao: %d\n", divisao(n1, n2));
+
+}*/
+
+
+
 
 
 
